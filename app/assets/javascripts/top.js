57a3093fed7e4011934bf71569fde44ffd246544
TomozQ <inf.4clover@gmail.com>
2020-08-26 01:43:28 +0900

    head__underのJS完成

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,15 +1,30 @@
 $(function(){
   // ▼マウスが載ったらサブメニューを表示
   $(".head__under__wrapper__cat").mouseenter(function(){
-     $(".head__under__in-brand").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
      $(".head__under__in-category").slideDown(150);     // 自分のサブメニューを表示する。
      $('.head__under__wrapper__cat').css('background-color', '#ffd9eb')
      $('.head__under__wrapper__cat').css('color', '#ff59c3')
+     $(".head__under__in-brand").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
+     $('.head__under__wrapper__bra').css('background-color', '#ff59c3')
+     $('.head__under__wrapper__bra').css('color', 'white')
   });
+
+  $(".head__under__wrapper__bra").mouseenter(function(){
+    $(".head__under__in-brand").slideDown(150);     // 自分のサブメニューを表示する。
+    $('.head__under__wrapper__bra').css('background-color', '#ffd9eb')
+    $('.head__under__wrapper__bra').css('color', '#ff59c3')
+    $(".head__under__in-category").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
+    $('.head__under__wrapper__cat').css('background-color', '#ff59c3')
+     $('.head__under__wrapper__cat').css('color', 'white')
+ });
+
   // ▼どこかがクリックされたらサブメニューを消す
   $('html').click(function() {
      $('.head__under__in-category').slideUp(150);
+     $('.head__under__in-brand').slideUp(150);
      $('.head__under__wrapper__cat').css('background-color', '#ff59c3')
      $('.head__under__wrapper__cat').css('color', 'white')
+     $('.head__under__wrapper__bra').css('background-color', '#ff59c3')
+     $('.head__under__wrapper__bra').css('color', 'white')
   });
 });
