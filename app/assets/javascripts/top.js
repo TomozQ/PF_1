98b0812652b7b8ada1b630dd25cfbce509b1a538
TomozQ <inf.4clover@gmail.com>
2020-08-26 16:10:21 +0900

    slideUp,slideDownの時間を変更

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,7 +1,7 @@
 $(function(){
   // ▼マウスが載ったらサブメニューを表示
   $(".head__under__wrapper__cat").mouseenter(function(){
-     $(".head__under__in-category").slideDown(150);     // 自分のサブメニューを表示する。
+     $(".head__under__in-category").slideDown(300);     // 自分のサブメニューを表示する。
      $('.head__under__wrapper__cat').css('background-color', '#ffd9eb')
      $('.head__under__wrapper__cat').css('color', '#ff59c3')
      $(".head__under__in-brand").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
@@ -10,7 +10,7 @@ $(function(){
   });
 
   $(".head__under__wrapper__bra").mouseenter(function(){
-    $(".head__under__in-brand").slideDown(150);     // 自分のサブメニューを表示する。
+    $(".head__under__in-brand").slideDown(300);     // 自分のサブメニューを表示する。
     $('.head__under__wrapper__bra').css('background-color', '#ffd9eb')
     $('.head__under__wrapper__bra').css('color', '#ff59c3')
     $(".head__under__in-category").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
@@ -20,8 +20,8 @@ $(function(){
 
   // ▼どこかがクリックされたらサブメニューを消す
   $('html').click(function() {
-     $('.head__under__in-category').slideUp(150);
-     $('.head__under__in-brand').slideUp(150);
+     $('.head__under__in-category').slideUp(300);
+     $('.head__under__in-brand').slideUp(300);
      $('.head__under__wrapper__cat').css('background-color', '#ff59c3')
      $('.head__under__wrapper__cat').css('color', 'white')
      $('.head__under__wrapper__bra').css('background-color', '#ff59c3')
